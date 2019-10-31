310645126c0ac13df0c5aee9599e0c5adfea7ab2
HarrietApel <harryapel1@gmail.com>
2019-10-31 11:15:56 +0000

    addin extra test function Answers UCL-RITS/rsd-classwork-2019#25

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -1,10 +1,15 @@
-from times import overlap_time
+from times import overlap_time, time_range
 
 def test_given_input():
-    if __name__ == "__main__":
-        large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
-        short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
-        result = overlap_time(large, short) 
-        expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
-        assert result == expected
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = overlap_time(large, short) 
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
 
+def test_class_time():
+    large = time_range("2010-10-31 10:00:00", "2010-10-31 13:00:00")
+    short = time_range("2010-10-31 10:05:00", "2010-10-31 12:55:00", 3, 600)
+    result = overlap_time(large, short) 
+    expected = [('2010-10-31 10:05:00', '2010-10-31 10:55:00'), ('2010-10-31 11:05:00', '2010-10-31 11:55:00'),('2010-10-31 12:05:00', '2010-10-31 12:55:00')]
+    assert result == expected
